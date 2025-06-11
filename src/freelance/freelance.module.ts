8ac9df818ffd997ff2834aeb1ad515a2ffd633af
baheddinefatnassi <baheddinefatnassi2001@gmail.com>
2025-06-11 09:40:04 +0100

    Document

--- src/freelance/freelance.module.ts
+++ src/freelance/freelance.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { FreelanceResolver } from './freelance.resolver';
 import { FreelanceService } from './freelance.service';
 import { Freelance } from './freelance.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Freelance])],
-  providers: [FreelanceService],
-  exports: [FreelanceService], // if needed by other modules
+  providers: [FreelanceResolver, FreelanceService],
+  exports: [FreelanceService],
 })
 export class FreelanceModule {}
